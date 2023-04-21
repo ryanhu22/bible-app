8a1bef4c52abcf7789a88d587d17ff747fee08ec
ryanhu22 <ryanhu22@berkeley.edu>
2023-04-21 16:45:50 -0700

    setting up next.js for prod

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b2575a506f46f524"; }
+/******/ 		__webpack_require__.h = function() { return "dd98b9f500b36db9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
