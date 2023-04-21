46cd42f6857bdf44b9cdccaf5ac23f0aeb96a664
ryanhu22 <ryanhu22@berkeley.edu>
2023-04-20 23:32:17 -0700

    cleaned package.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "43fda4316f22083a"; }
+/******/ 		__webpack_require__.h = function() { return "4825d6e5979be8bf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.d79eeb8738d8788c.hot-update.js
+++ .next/static/webpack/webpack.d79eeb8738d8788c.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "1a13575ad44237da"; }
+/******/ 	__webpack_require__.h = function() { return "4825d6e5979be8bf"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
